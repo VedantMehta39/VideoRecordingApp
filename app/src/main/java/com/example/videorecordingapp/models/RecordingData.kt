b5ef5b3b7b01584b9fd133a62083fd2d834aaf8e
VedantMehta39 <42540089+VedantMehta39@users.noreply.github.com>
2020-10-03 02:34:12 -0700

    Add Model file to stored recording data

--- app/src/main/java/com/example/videorecordingapp/models/RecordingData.kt
+++ app/src/main/java/com/example/videorecordingapp/models/RecordingData.kt
@@ -2,4 +2,7 @@ package com.example.videorecordingapp.models
 
 import java.sql.Timestamp
 
-data class RecordingData(val name:String, val duration:Int, val timestamp:Timestamp)
+const val MIN_RECORDING_TIME = 15
+
+
+data class RecordingData(val name:String = "", val duration:Int = MIN_RECORDING_TIME, val timestamp:Timestamp? = null)
